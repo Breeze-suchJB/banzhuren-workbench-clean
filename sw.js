2f375165d878cf49d1df409dd017860b8cb6bc79
Breeze-suchJB <316904978+Breeze-suchJB@users.noreply.github.com>
2026-08-14 18:51:44 +0800

    补充 PWA manifest 与图标（修复演示版无法安装）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 班主任智能工作台 Service Worker */
-const CACHE_NAME = 'banzhuren-workbench-20260814-184956';
+const CACHE_NAME = 'banzhuren-workbench-20260814-185128';
 const ASSETS = [
   './',
   './index.html',
